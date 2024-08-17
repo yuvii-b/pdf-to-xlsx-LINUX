92bb852b735b05b3f749fb9d51635f0759f7f782
Yuvaraj <yuvaraj.codes@gmail.com>
2024-08-17 19:06:13 +0530

    Update script.py - improved code

--- script.py
+++ script.py
@@ -1,19 +1,23 @@
+import PyPDF2
 import csv
 import camelot
 
-heading = [] # The row heading of the tablular data in the pdf data is given here as a list of strings
-with open('csvdata.csv', 'w') as f:
-    writer = csv.writer(f, delimiter=',')
-    writer.writerow(heading)
+pdf_file = "list.pdf"
+read = PyPDF2.PdfReader(pdf_file)
+pages = len(read.pages)
 
-starting_page = 1 # Denotes the first page, page from which the table in pdf is to be converted into csv(xls)
-ending_page = 2 # Denotes the last page, page upto which the table in pdf is to be converted into csv(xls)
-pdf_file = '' # The name or path(if the file if not in the same directory) of the pdf is to be given here
-for i in range(starting_page, ending_page + 1):
+# pages = int(str(file)[13:].replace(">", ""))
+
+for i in range(pages + 1):
     file = camelot.read_pdf(pdf_file, pages="{}".format(i))
     lst = file[0].df.values.tolist()
-    with open('csvdata.csv', 'a') as f:
+    if(i == 0):
+        heading = [s.replace("\n", "") for s in file[0].df.values.tolist()[0]]
+        with open('csvdata.csv', 'w') as f:
+            writer = csv.writer(f, delimiter=',')
+            writer.writerow(heading)
+    with open("csvdata.csv", "a") as f:
         writer = csv.writer(f, delimiter=',')
-        for line in lst[1:len(lst) + 1]:
+        for line in lst[1:len(lst)+1]:
             writer.writerow(line)
 f.close()
